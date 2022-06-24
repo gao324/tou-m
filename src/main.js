5c1a1073b909d014c46038d51f5a0b13f922671b
高先生 <106066550+gao324@users.noreply.github.com>
2022-06-24 19:27:56 +0800

    项目初始化-vant引入

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
-
+// 引入vant组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
 Vue.config.productionTip = false
+
+Vue.use(Vant)
 
 new Vue({
   router,
